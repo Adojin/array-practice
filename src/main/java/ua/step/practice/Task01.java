c88dbb3411eb01551c5ce2ab1fd820a832736a92
Adojin <71118113+adojin@users.noreply.github.com>
2020-09-18 11:42:56 +0300

    решено первое задание

--- src/main/java/ua/step/practice/Task01.java
+++ src/main/java/ua/step/practice/Task01.java
@@ -9,8 +9,16 @@ package ua.step.practice;
  */
 public class Task01 {
     public static void main(String[] args) {
-        // TODO: Пишите код здесь
-
-
+        int mas[]= new int[15];
+        mas [0] = 1;
+        mas [1] = 1;
+        for (int i = 2; i < mas.length; i++) {
+            mas[i] = mas[i - 1] + mas[i - 2];
+        }
+        for(int i = 0; i < mas.length; i++) {
+            if (i % 2 == 0) {
+                System.out.print(mas[i] + " ");
+            }
+        }
     }
 }
